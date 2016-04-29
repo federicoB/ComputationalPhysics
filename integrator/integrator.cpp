7d403c55ee1fd4016d392dda831c83e2e7926f80
federicoB <federicobertani96@gmail.com>
2016-04-29 17:07:09 +0200

    improved documentation

--- integrator/integrator.cpp
+++ integrator/integrator.cpp
@@ -7,59 +7,84 @@
 
 
 double f(const double &x) {
+    //sample function
     return sin(x);
 }
 
 //Reference for methods https://en.wikipedia.org/wiki/Numerical_integration#Quadrature_rules_based_on_interpolating_functions
 
-double rectangle(double a, double b, int N) {
-    double sum = 0.0;
-    double dx = (b - a) / N;
-
-    for (int i = 0; i < N; i++) {
-        double xi = a + (dx * i);
-        sum += dx * f(xi);
+double rectangle(double leftExtreme, double rightExtreme, int numberOfSegments) {
+    //declare a variable sum for containg the sum of the areas of the rectangles
+    double sum = 0;
+    //determine the rectangble base subdividing the leght of the interval for the desired numberOfSegments
+    double rectancleBase = (rightExtreme - leftExtreme) / numberOfSegments;
+    //for the numberOfSegments
+    for (int i = 0; i < numberOfSegments; i++) {
+        //determine the x-cordinate, left-bottom vertex of the rectangle
+        double leftBottomVertexPosition = leftExtreme + (rectancleBase * i);
+        //determine the area of the rectangle
+        sum += rectancleBase * f(leftBottomVertexPosition);
     }
-
+    //return the calculated sum
     return sum;
 }
 
 
-double trapezoidal(double a, double b, int N) {
+double trapezoidal(double leftExtreme, double rightExtreme, int numberOfSegments) {
+    //declare a variable sum for containg the sum of the areas of the traperzoids
     double sum = 0.0;
-    double dx = (b - a) / N;
-
-    for (int i = 0; i < N; i++) {
-        double xi = a + (dx * i);
-        double xi2 = a + (dx * (i+1));
-        double area = ((f(xi)+f(xi2))*dx)/2;
+    //determine the trapezoid base subdividing the leght of the interval for the desired numberOfSegments
+    double trapezoidBase = (rightExtreme - leftExtreme) / numberOfSegments;
+    //for all the numberOfSegments
+    for (int i = 0; i < numberOfSegments; i++) {
+        //determine the position of the bottom-left vertex of the trapezoid
+        double bottomLeftVertex = leftExtreme + (trapezoidBase * i);
+        //determine the position of the bottom-right verted of the trapezoid
+        double bottomRightVertex = leftExtreme + (trapezoidBase * (i+1));
+        //determine the area with the forumala: ((small base + big base)*height)/2
+        double area = ((f(bottomLeftVertex)+f(bottomRightVertex))*trapezoidBase)/2;
+        //increase the sum of the areas
         sum += area;
     }
-
+    //return the calculated sum
     return sum;
 }
 
 double simpson(double a, double b, int N) {
-    //DA FARE
+    //TODO implement simpson method
     return 0.0;
 }
 
 int main() {
+    //create an output stream to a file
     std::ofstream outputFile("outputFile.txt");
+    //if the file is open
     if (outputFile.is_open()) {
+        //determine the real value of the sine on 1 (the cose is always complementary to it)
         double truth = 1.0 - cos(1.0);
+        //create a legend for the output file
         outputFile << "A   \t B\t\t\tC" << std::endl;
-        for (int n = 1; n < 100; n++) //calcolo degli integrali con varie discretizzazioni(segmenti)
+        //loop for varius number of segments
+        for (int n = 1; n < 100; n++)
         {
-            int N = n * n; //numero di segmenti
+            //increase exponentially the number of segments
+            int N = n * n;
+            //set the output in scientific notation
             outputFile.setf(std::ios_base::scientific);
+            //set the decimal precision to 2
             outputFile.precision(2);
-            outputFile << N << "   \t" << rectangle(0.0, 1.0, N) - truth /*errore con il metodo dei rectangle*/ << "\t" <<
-                    trapezoidal(0.0, 1.0, N) - truth << "\t" << simpson(0.0, 1.0, N) - truth << std::endl;
+            //print current number of segments
+            outputFile << N << "   \t"
+            //error with rectangle method
+            << rectangle(0.0, 1.0, N) - truth << "\t"
+            //error with trapezoidal method
+            << trapezoidal(0.0, 1.0, N) - truth << "\t"
+            //error with simpson method
+            << simpson(0.0, 1.0, N) - truth << std::endl;
         }
 
     }
+    //close the output file
     outputFile.close();
-
     return 0;
 }
--- integrator/integrator.h
+++ integrator/integrator.h
@@ -11,9 +11,9 @@
 double f(const double &x);
 
 //Reference for methods https://en.wikipedia.org/wiki/Numerical_integration#Quadrature_rules_based_on_interpolating_functions
-double rectangle(double a, double b, int N);
+double rectangle(double leftExtreme, double rightExtreme, int numberOfSegments);
 
-double trapezoidal(double a, double b, int N);
+double trapezoidal(double leftExtreme, double rightExtreme, int numberOfSegments);
 
 double simpson(double a, double b, int N);
 
