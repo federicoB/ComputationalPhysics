78dd6f07769f55706dc2c27e63f05eabc41ca63a
federicoB <federicobertani96@gmail.com>
2016-06-07 12:58:51 +0200

    Generalized integrator function with function pointers

--- integrator/integrator.cpp
+++ integrator/integrator.cpp
@@ -5,7 +5,9 @@
 #include <iostream>
 #include "integrator.h"
 
-
+/**
+ * An example funtion to integrate.
+ */
 double f(const double &x) {
     //sample function
     return sin(x);
@@ -13,7 +15,8 @@ double f(const double &x) {
 
 //Reference for methods https://en.wikipedia.org/wiki/Numerical_integration#Quadrature_rules_based_on_interpolating_functions
 
-double rectangle(double leftExtreme, double rightExtreme, int numberOfSegments) {
+double rectangle(double leftExtreme, double rightExtreme, int numberOfSegments,
+                 double (*functionToIntegrate)(const double &)) {
     //declare a variable sum for containg the sum of the areas of the rectangles
     double sum = 0;
     //determine the rectangble base subdividing the leght of the interval for the desired numberOfSegments
@@ -23,14 +26,15 @@ double rectangle(double leftExtreme, double rightExtreme, int numberOfSegments) 
         //determine the x-cordinate, left-bottom vertex of the rectangle
         double leftBottomVertexPosition = leftExtreme + (rectancleBase * i);
         //determine the area of the rectangle
-        sum += rectancleBase * f(leftBottomVertexPosition);
+        sum += rectancleBase * functionToIntegrate(leftBottomVertexPosition);
     }
     //return the calculated sum
     return sum;
 }
 
 
-double trapezoidal(double leftExtreme, double rightExtreme, int numberOfSegments) {
+double trapezoidal(double leftExtreme, double rightExtreme, int numberOfSegments,
+                   double (*functionToIntegrate)(const double &)) {
     //declare a variable sum for containg the sum of the areas of the traperzoids
     double sum = 0.0;
     //determine the trapezoid base subdividing the leght of the interval for the desired numberOfSegments
@@ -40,9 +44,9 @@ double trapezoidal(double leftExtreme, double rightExtreme, int numberOfSegments
         //determine the position of the bottom-left vertex of the trapezoid
         double bottomLeftVertex = leftExtreme + (trapezoidBase * i);
         //determine the position of the bottom-right verted of the trapezoid
-        double bottomRightVertex = leftExtreme + (trapezoidBase * (i+1));
+        double bottomRightVertex = leftExtreme + (trapezoidBase * (i + 1));
         //determine the area with the forumala: ((small base + big base)*height)/2
-        double area = ((f(bottomLeftVertex)+f(bottomRightVertex))*trapezoidBase)/2;
+        double area = ((f(bottomLeftVertex) + functionToIntegrate(bottomRightVertex)) * trapezoidBase) / 2;
         //increase the sum of the areas
         sum += area;
     }
@@ -52,7 +56,8 @@ double trapezoidal(double leftExtreme, double rightExtreme, int numberOfSegments
 
 //reference https://en.wikipedia.org/wiki/Simpson%27s_rule#Composite_Simpson.27s_rule
 
-double simpson(double leftExtreme, double rightExtreme, int numberOfSegments) {
+double simpson(double leftExtreme, double rightExtreme, int numberOfSegments,
+               double (*functionToIntegrate)(const double &)) {
     //declare a variable sum for containg the sum of the areas of the parabola
     double sum = 0.0;
     //determine the segmentLenght base subdividing the leght of the interval for the desired numberOfSegments
@@ -60,19 +65,21 @@ double simpson(double leftExtreme, double rightExtreme, int numberOfSegments) {
     bool even = false;
     double point = leftExtreme;
     //for all the numberOfSegments
-    for (int i = 1; i < numberOfSegments/2; i++) {
-        double point1 = leftExtreme + segmentLeght*((2*i)-2);
-        double point2 = leftExtreme + segmentLeght*((2*i)-1);
-        double point3 = leftExtreme + segmentLeght*((2*i));
-        sum += f(point1) + 4*f(point2)+f(point3);
+    for (int i = 1; i < numberOfSegments / 2; i++) {
+        double point1 = leftExtreme + segmentLeght * ((2 * i) - 2);
+        double point2 = leftExtreme + segmentLeght * ((2 * i) - 1);
+        double point3 = leftExtreme + segmentLeght * ((2 * i));
+        sum += f(point1) + 4 * f(point2) + functionToIntegrate(point3);
     }
-    sum = sum * (segmentLeght/3);
+    sum = sum * (segmentLeght / 3);
     return sum;
 }
 
 int main() {
     //create an output stream to a file
     std::ofstream outputFile("outputFile.txt");
+    //create a function pointer for the function to integrate
+    double (*function)(const double &) = &f;
     //if the file is open
     if (outputFile.is_open()) {
         //determine the real value of definite integral of the sine from 0 to 1 with the antiderivative.
@@ -80,8 +87,7 @@ int main() {
         //create a legend for the output file
         outputFile << "A   \t B\t\t\tC" << std::endl;
         //loop for varius number of segments
-        for (int n = 1; n < 100; n++)
-        {
+        for (int n = 1; n < 100; n++) {
             //increase exponentially the number of segments
             int N = n * n;
             //set the output in scientific notation
@@ -91,11 +97,11 @@ int main() {
             //print current number of segments
             outputFile << N << "   \t"
             //error with rectangle method
-            << rectangle(0, 1, N) - truth  << "\t"
+            << rectangle(0, 1, N, function) - truth << "\t"
             //error with trapezoidal method
-            << trapezoidal(0, 1, N) - truth << "\t"
+            << trapezoidal(0, 1, N, function) - truth << "\t"
             //error with simpson method
-            << simpson(0, 1, N)  - truth << std::endl;
+            << simpson(0, 1, N, function) - truth << std::endl;
         }
 
     }
--- integrator/integrator.h
+++ integrator/integrator.h
@@ -5,19 +5,14 @@
 #ifndef INTEGRATOR_H
 #define INTEGRATOR_H
 
-/**
- * The function to integrate
- */
-double f(const double &x);
-
 //Reference for methods https://en.wikipedia.org/wiki/Numerical_integration#Quadrature_rules_based_on_interpolating_functions
 
-double rectangle(double leftExtreme, double rightExtreme, int numberOfSegments);
+double rectangle(double leftExtreme, double rightExtreme, int numberOfSegments, double (*functionToIntegrate)(const double &));
 
-double trapezoidal(double leftExtreme, double rightExtreme, int numberOfSegments);
+double trapezoidal(double leftExtreme, double rightExtreme, int numberOfSegments, double (*functionToIntegrate)(const double &));
 
 //reference https://en.wikipedia.org/wiki/Simpson%27s_rule#Composite_Simpson.27s_rule
 
-double simpson(double a, double b, int N);
+double simpson(double a, double b, int N, double (*functionToIntegrate)(const double &));
 
 #endif //INTEGRATOR_H
