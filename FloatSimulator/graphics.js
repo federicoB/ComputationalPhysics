ede8a003d5c230dd75d2814671fdab6fbb990d4a
federicoB <federicobertani96@gmail.com>
2016-09-19 20:40:28 +0200

    Added documentations for the ground Graphic class

--- FloatSimulator/graphics.js
+++ FloatSimulator/graphics.js
@@ -1,37 +1,67 @@
 //The graphic is a virtual object, it's the design of the shape.
 
+/**
+ * The graphic representing the ground. A ground can have multiple holes in it.
+ */
 class GroundGraphic extends createjs.Graphics {
+    //create an empty groundgraphic
     constructor() {
+        //call the super constructor for creating createjs.Graphics base class
         super();
+        //create an empty array for cointaing the holes
         this.holes = [];
     }
+    //call this when ready to draw the ground
     drawGraphic() {
+        //get the width of the canvas and save it on a local variable
         var width = view.width;
+        //get the height of the canvas and save it on a local variable
         var height = view.height;
-        var x=0,y=height/2;
-        //start of the ground
+        //define a local variable for containing the current abscissa of drawing point. Initialize it to 0.
+        var x=0;
+        //define a local variable for containing the current ordinate of drawing point. Initialize it to half the height of the canvas.
+        var y=height/2;
+        //move the drawing point to the initial position and start filling the graphic with green color
         this.moveTo(x, y).beginFill("green");
+        //for every hole object in the holes array
         for (var holeObject of this.holes) {
+            //set the abscissa to the leftmargin of the hole
             x=holeObject.leftMargin;
             this.lineTo(x,y);
+            //move the point to the left bottom of the hole
             y+=holeObject.hole.depth;
             this.lineTo(x,y);
+            //move the point to the right bottom of the hole
             x+=holeObject.hole.width;
             this.lineTo(x,y);
+            //move the point to the right top of the hole
             y-=holeObject.hole.depth;
             this.lineTo(x,y);
         }
+        //set tha absissa to the end of the ground
         x=width;
         this.lineTo(x,y);
+        //draw a line to close the right part of the ground
         y=height;
         this.lineTo(x,y);
+        //draw a line to close the bottom part of the ground
         x=0;
         this.lineTo(x,y);
+        //draw a line to close the left part of the ground
         y=height/2;
         this.lineTo(x,y);
+        //close the drawn path and complete the filling
         this.closePath().endFill();
+        //return this for method chaining
         return this;
     }
+
+    /**
+     * Add a hole to the ground graphic object
+     * @param hole: the object representing the hole. It must have properties for width and depth
+     * @param leftMargin: the leftmargin of the hole from the left start of the ground
+     * @returns {GroundGraphic} return the ground graphic for method chaining
+     */
     addHole(hole,leftMargin) {
         this.holes.push({hole : hole,leftMargin : leftMargin});
         //TODO sort holes array or insert in right position
@@ -83,6 +113,6 @@ class BoxGraphic extends createjs.Graphics {
         super();
         this.width = width;
         this.height = height;
-        this.beginFill("#993300").drawRect(0,0,width,height);
+        this.beginFill("#993300").drawRect(0,0,width,height).endFill();
     }
 }
